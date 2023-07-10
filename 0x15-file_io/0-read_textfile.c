512db5b1f2fc2a7f83651405ecaf9282c89df9b6
Emmie05 <emmvicadforster@gmail.com>
2023-07-10 15:01:14 -0700

    C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,17 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
  * read_textfile - Read text file and print to STDOUT.
  * @filename: Text file being read.
  * @letters: Number of letters to be read.
- * Return: Actual number of bytes read and printed, 0 when function fails or
- *         filename is NULL.
+ * Return: Actual number of letters read and printed, 0 when the function fails or
+ *         when filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
 	char *buf;
-	ssize_t fd, w, t;
+	ssize_t t, w;
+
+	if (filename == NULL)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -19,15 +26,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
+	{
+		close(fd);
 		return (0);
+	}
 
 	t = read(fd, buf, letters);
+	if (t == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
 	w = write(STDOUT_FILENO, buf, t);
+	if (w == -1 || t != w)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
 	free(buf);
 	close(fd);
-
-	if (t != w || w == -1)
-		return (0);
 	return (w);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,10 @@
 
 #include <elf.h>
 #include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
@@ -11,9 +15,5 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 int main(int argc, char **argv);
-
-void print_error(const char *message);
-void print_elf_header(const Elf64_Ehdr *header);
-int read_elf_header(const char *filename, Elf64_Ehdr *header);
 
 #endif
