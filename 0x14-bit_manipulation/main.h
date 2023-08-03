c5dcf77f46562408dfc4b92a8386cef87e6c4315
Emmie05 <emmvicadforster@gmail.com>
2023-08-03 08:44:56 -0700

    Bit_Manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,6 +9,7 @@ unsigned int binary_to_uint(const char *b);
 
 /* Task 1 - Print Binary */
 void print_binary(unsigned long int n);
+int _putchar(char c);
 
 /* Task 2 - Get Bit */
 int get_bit(unsigned long int n, unsigned int index);
