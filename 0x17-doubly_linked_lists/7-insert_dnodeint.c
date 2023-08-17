c09a8830cf0c2e01ec4887b23665a9ec4a32db5c
Emmie05 <emmvicadforster@gmail.com>
2023-08-17 16:14:23 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,56 +4,49 @@
 /**
  * insert_dnodeint_at_index - Inserts a new node at a given position.
  * @h: Double pointer to the head of the list.
- * @idx: Index where the new node should be added. Index starts at 0.
- * @n: Value to be assigned to the new node.
- * Return: The address of the new node, or NULL if it failed.
+ * @idx: Index of the list where the new node be added. Index starts at 0.
+ * @n: Value to store in the new node.
+ * Return: Address of the new node, or NULL if it failed.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-    {
-        return (NULL);  // Memory allocation failed
-    }
+	dlistint_t *new_node, *temp = *h;
+	unsigned int i = 0;
 
-    new_node->n = n;
+	if (h == NULL)
+		return (NULL);
 
-    if (idx == 0)
-    {
-        new_node->prev = NULL;
-        new_node->next = *h;
-        if (*h != NULL)
-        {
-            (*h)->prev = new_node;
-        }
-        *h = new_node;
-        return (new_node);
-    }
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    dlistint_t *current = *h;
-    unsigned int count = 0;
+	new_node->n = n;
 
-    while (current != NULL && count < idx - 1)
-    {
-        current = current->next;
-        count++;
-    }
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		new_node->prev = NULL;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
 
-    if (current == NULL)
-    {
-        free(new_node);
-        return (NULL); 
-    }
+	while (temp != NULL)
+	{
+		if (i == idx - 1)
+		{
+			new_node->next = temp->next;
+			new_node->prev = temp;
+			if (temp->next != NULL)
+				temp->next->prev = new_node;
+			temp->next = new_node;
+			return (new_node);
+		}
+		temp = temp->next;
+		i++;
+	}
 
-    new_node->prev = current;
-    new_node->next = current->next;
-
-    if (current->next != NULL)
-    {
-        current->next->prev = new_node;
-    }
-
-    current->next = new_node;
-
-    return (new_node);
+	free(new_node);
+	return (NULL);
 }
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,54 +2,43 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Deletes the node at a given index.
+ * delete_dnodeint_at_index - Deletes node at index of a linked list.
  * @head: Double pointer to the head of the list.
- * @index: Index of the node that should be deleted. Index starts at 0.
+ * @index: Index of the node to be deleted. Index starts at 0.
  * Return: 1 if it succeeded, -1 if it failed.
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    if (*head == NULL)
-    {
-        return (-1);  // Empty list, deletion failed
-    }
+	dlistint_t *current = *head;
+	unsigned int i = 0;
 
-    dlistint_t *current = *head;
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    if (index == 0)
-    {
-        *head = (*head)->next;
-        if (*head != NULL)
-        {
-            (*head)->prev = NULL;
-        }
-        free(current);
-        return (1);
-    }
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
 
-    unsigned int count = 0;
+	while (current != NULL)
+	{
+		if (i == index)
+		{
+			if (current->prev != NULL)
+				current->prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = current->prev;
+			free(current);
+			return (1);
+		}
+		current = current->next;
+		i++;
+	}
 
-    while (current != NULL && count < index)
-    {
-        current = current->next;
-        count++;
-    }
-
-    if (current == NULL)
-    {
-        return (-1);  // Cannot delete the node at the specified index
-    }
-
-    if (current->next != NULL)
-    {
-        current->next->prev = current->prev;
-    }
-
-    if (current->prev != NULL)
-    {
-        current->prev->next = current->next;
-    }
-
-    free(current);
-    return (1);
+	return (-1);
 }
+
